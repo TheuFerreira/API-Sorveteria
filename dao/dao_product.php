a2c705526c2ddb51afd069b8aa54a8399412f9c5
Matheus Ferreira <matheus.44300@gmail.com>
2022-06-04 14:57:19 -0300

    Get products by category

--- dao/dao_product.php
+++ dao/dao_product.php
@@ -17,6 +17,26 @@ class DAOProduct {
         $result = $psd->fetchAll(PDO::FETCH_BOTH);
         return $result;
     }
+
+    public function getByCategory(int $idCategory, string $search) {
+        $sql = '
+            SELECT p.id_product, p.title, p.price, p.path 
+            FROM product AS p
+            INNER JOIN category_product AS cp ON p.id_product = cp.id_product
+            WHERE p.title LIKE "%' . $search . '%"
+                AND p.status = 1
+                AND cp.id_category = ?
+            ORDER BY p.title
+            LIMIT 10;';
+
+        $connection = new Connection();
+        $psd = $connection->prepareSQL($sql);
+        $psd->bindValue(1, $idCategory);
+        $psd->execute();
+
+        $result = $psd->fetchAll(PDO::FETCH_BOTH);
+        return $result;
+    }
 }
 
 ?>
---
+++ routes/product/get_by_category.php
@@ -0,0 +1,37 @@
+<?php
+
+require '../../database/connection.php';
+require '../../dao/dao_product.php';
+
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+$search = $data->search;
+$idCategory = $data->id_category;
+
+$dao = new DAOProduct();
+$rows = $dao->getByCategory($idCategory, $search);
+
+$responses = array();
+for ($i = 0; $i < count($rows); $i++) {
+    $row = $rows[$i];
+
+    $response['idProduct'] = $row['id_product'];
+    $response['title'] = $row['title'];
+    $response['price'] = (float)$row['price'];
+
+    $path = '../../img/product/' . $row['path'];
+    if (file_exists($path)) {
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+        $response['img'] = $base64;
+    }
+
+    array_push($responses, $response);
+}
+
+echo json_encode($responses);
+
+?>
